dfe316a25ea2dd4ea72874a72bdff0de0d75e689
Syed Faraaz Ahmad <faraaz98@live.com>
2019-03-17 06:15:41 +0530

    add listener to server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,13 @@
 package main
 
+import "net"
+
 type server struct{}
 
 func main() {
-
+	// create a tcp listener at port 4040
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		panic(err)
+	}
 }
