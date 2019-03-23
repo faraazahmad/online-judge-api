af693b9ce2e41145839ee6fd89c206e91e4e3aa7
Syed Faraaz Ahmad <faraaz98@live.com>
2019-03-24 05:16:25 +0530

    add current time

--- client/main.go
+++ client/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"strings"
 
 	proto "../proto"
 	"github.com/gin-gonic/gin"
@@ -21,7 +24,29 @@ func main() {
 	/*
 		Get codeURL, args and stdin from body
 	*/
-	g.GET("/ruby", ruby.handler)
+	g.GET("/ruby", func(ctx *gin.Context) {
+		// get codeURL from request body
+		codeURL := ctx.PostForm("url")
+
+		// get args from request body and split into []string
+		args := strings.Split(ctx.PostForm("args"), ",")
+
+		// get stdin from request body
+		stdin := ctx.PostForm("stdin")
+
+		// create protobuf request
+		req := &proto.Request{CodeURL: codeURL, Args: args, Stdin: []byte(stdin)}
+
+		// send request and get response, error
+		if response, err := Ruby(ctx, req); err == nil {
+			ctx.JSON(http.StatusOK, gin.H{
+				"result": fmt.Sprintf("%s", response.Body),
+			})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error()})
+		}
+	})
 
 	if err := g.Run(":8080"); err != nil {
 		log.Fatalf("Failed to run server: %v", err)
--- server/main.go
+++ server/main.go
@@ -1,14 +1,72 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
 	"net"
+	"os"
+	"os/exec"
+	"os/user"
+	"time"
 
 	proto "../proto"
+	"../wget"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 type server struct{}
+
+func (s *server) Ruby(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	// extract the code URL and its arguments from the request
+	codeURL, args := request.GetCodeURL(), request.GetArgs()
+
+	/*
+		Code has to be downloaded in
+		/home/${whoami}/remote/ruby/temp.rb
+	*/
+
+	// get home directory of current user
+	homeDir, err := user.Current()
+	if err != nil {
+		return nil, err
+	}
+
+	// get and format current time for every request
+	t := time.Now().Format("20060102150405")
+	// generate string for destination (in UNIX based systems)
+	destinationString := fmt.Sprintf("%s/remote/ruby/request-%s/code.rb", homeDir, t)
+
+	// download file in the provided destination
+	wget.Wget(codeURL, destinationString)
+
+	// get Command struct instance by passing command name and arguments
+	cmd := exec.Command("ruby", args...)
+
+	// provide stdin to command
+	cmd.Stdin = bytes.NewReader(request.GetStdin())
+
+	// store cmd.Stdout in a Bytes buffer
+	var Stdout bytes.Buffer
+	cmd.Stdout = &Stdout
+
+	// run the command
+	err = cmd.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	// delete the code file
+	err = os.Remove(destinationString)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// return full response
+	return &proto.Response{Body: Stdout.Bytes()}, nil
+}
 
 func main() {
 	// create a tcp listener at port 4040
