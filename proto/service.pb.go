20b76cca4b97aa77c429eadeddfcfe76ed255ea5
Syed Faraaz Ahmad <faraaz98@live.com>
2019-05-25 18:29:19 +0530

    fix all errors after refactor

--- proto/service.pb.go
+++ proto/service.pb.go
@@ -6,11 +6,10 @@ package service
 import (
 	context "context"
 	fmt "fmt"
+	math "math"
+
 	proto "github.com/golang/protobuf/proto"
 	grpc "google.golang.org/grpc"
-	codes "google.golang.org/grpc/codes"
-	status "google.golang.org/grpc/status"
-	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -126,7 +125,7 @@ func init() {
 func init() { proto.RegisterFile("service.proto", fileDescriptor_a0b84a42fa06f626) }
 
 var fileDescriptor_a0b84a42fa06f626 = []byte{
-	// 163 bytes of a gzipped FileDescriptorProto
+	// 181 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x2d, 0x4e, 0x2d, 0x2a,
 	0xcb, 0x4c, 0x4e, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x57, 0xf2, 0xe5, 0x62, 0x0f, 0x4a, 0x2d,
 	0x2c, 0x4d, 0x2d, 0x2e, 0x11, 0x92, 0xe0, 0x62, 0x4f, 0xce, 0x4f, 0x49, 0x0d, 0x0d, 0xf2, 0x91,
@@ -134,10 +133,11 @@ var fileDescriptor_a0b84a42fa06f626 = []byte{
 	0x25, 0x98, 0x14, 0x98, 0x35, 0x38, 0x83, 0xc0, 0x6c, 0x21, 0x11, 0x2e, 0xd6, 0xe2, 0x92, 0x94,
 	0xcc, 0x3c, 0x09, 0x66, 0x05, 0x46, 0x0d, 0x9e, 0x20, 0x08, 0x47, 0x49, 0x8e, 0x8b, 0x23, 0x28,
 	0xb5, 0xb8, 0x20, 0x3f, 0xaf, 0x38, 0x15, 0xa4, 0x2b, 0x29, 0x3f, 0xa5, 0x52, 0x82, 0x09, 0xac,
-	0x00, 0xcc, 0x36, 0xd2, 0xe2, 0xe2, 0x76, 0xad, 0x48, 0x4d, 0x0e, 0x86, 0xb8, 0x41, 0x48, 0x9a,
-	0x8b, 0x25, 0xa8, 0x34, 0xa9, 0x52, 0x88, 0x43, 0x0f, 0xea, 0x08, 0x29, 0x4e, 0x3d, 0x98, 0xfe,
-	0x24, 0x36, 0xb0, 0x0b, 0x8d, 0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0x74, 0xa8, 0x4b, 0x3d, 0xb2,
-	0x00, 0x00, 0x00,
+	0x00, 0xcc, 0x36, 0x0a, 0xe2, 0xe2, 0x76, 0xad, 0x48, 0x4d, 0x0e, 0x86, 0xb8, 0x41, 0x48, 0x89,
+	0x8b, 0xdb, 0x33, 0xaf, 0x24, 0xb5, 0xa8, 0xa0, 0x28, 0xb5, 0x24, 0x35, 0x45, 0x88, 0x43, 0x0f,
+	0xea, 0x16, 0x29, 0x4e, 0x3d, 0xb8, 0x31, 0xf2, 0x5c, 0x1c, 0xce, 0xf9, 0xb9, 0x05, 0x99, 0x39,
+	0x38, 0x14, 0x24, 0xb1, 0x81, 0x7d, 0x62, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0xda, 0x4a, 0x05,
+	0x74, 0xda, 0x00, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -152,7 +152,8 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type ExecServiceClient interface {
-	Ruby(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
+	Interpreted(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
+	Compiled(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
 }
 
 type execServiceClient struct {
@@ -163,9 +164,18 @@ func NewExecServiceClient(cc *grpc.ClientConn) ExecServiceClient {
 	return &execServiceClient{cc}
 }
 
-func (c *execServiceClient) Ruby(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
+func (c *execServiceClient) Interpreted(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
 	out := new(Response)
-	err := c.cc.Invoke(ctx, "/ExecService/Ruby", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/ExecService/Interpreted", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *execServiceClient) Compiled(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
+	err := c.cc.Invoke(ctx, "/ExecService/Compiled", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -174,35 +184,46 @@ func (c *execServiceClient) Ruby(ctx context.Context, in *Request, opts ...grpc.
 
 // ExecServiceServer is the server API for ExecService service.
 type ExecServiceServer interface {
-	Ruby(context.Context, *Request) (*Response, error)
-}
-
-// UnimplementedExecServiceServer can be embedded to have forward compatible implementations.
-type UnimplementedExecServiceServer struct {
-}
-
-func (*UnimplementedExecServiceServer) Ruby(ctx context.Context, req *Request) (*Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Ruby not implemented")
+	Interpreted(context.Context, *Request) (*Response, error)
+	Compiled(context.Context, *Request) (*Response, error)
 }
 
 func RegisterExecServiceServer(s *grpc.Server, srv ExecServiceServer) {
 	s.RegisterService(&_ExecService_serviceDesc, srv)
 }
 
-func _ExecService_Ruby_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _ExecService_Interpreted_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Request)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ExecServiceServer).Ruby(ctx, in)
+		return srv.(ExecServiceServer).Interpreted(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/ExecService/Ruby",
+		FullMethod: "/ExecService/Interpreted",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ExecServiceServer).Ruby(ctx, req.(*Request))
+		return srv.(ExecServiceServer).Interpreted(ctx, req.(*Request))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ExecService_Compiled_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Request)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ExecServiceServer).Compiled(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/ExecService/Compiled",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ExecServiceServer).Compiled(ctx, req.(*Request))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -212,8 +233,12 @@ var _ExecService_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*ExecServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "Ruby",
-			Handler:    _ExecService_Ruby_Handler,
+			MethodName: "Interpreted",
+			Handler:    _ExecService_Interpreted_Handler,
+		},
+		{
+			MethodName: "Compiled",
+			Handler:    _ExecService_Compiled_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
